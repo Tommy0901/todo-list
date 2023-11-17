9f23a939c1488ce0a41310b82ecc6c1dc671cf1d
Tommy0901 <ak710180@gmail.com>
2023-11-17 19:02:20 +0800

    add todo seeder

---
+++ seeders/20231117103123-todo-item.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "todos",
+      Array.from(Array(10)).map((_, i) => ({
+        name: `todo-${i}`,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }))
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("todos", null)
+  },
+};
