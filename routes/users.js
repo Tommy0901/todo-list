7eecc683f1032ec7f0b5e761732132094d8f4990
Tommy0901 <ak710180@gmail.com>
2023-12-04 12:07:02 +0800

    implement register

--- routes/users.js
+++ routes/users.js
@@ -17,9 +17,33 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res) => {
-  const { password, confirmPassword } = req.body;
-  if (password !== confirmPassword) throw new Error("Passwords do not match!");
-  return res.send(req.body);
+  const { email, name, password, confirmPassword } = req.body;
+
+  if (!name || !email || !password) {
+    req.flash("error", "name, email 及 password 為必填");
+    return res.redirect("back");
+  }
+
+  if (password !== confirmPassword) {
+    req.flash("error", "驗證密碼與密碼不符 :(");
+    return res.redirect("back");
+  }
+
+  (async () => {
+    try {
+      const rowCount = await User.count({ where: { email } });
+      if (rowCount > 0) {
+        req.flash("error", "email 已註冊 :(");
+        return res.redirect("back");
+      }
+      await User.create({ email, name, password });
+      req.flash("success", "註冊成功!");
+      return res.redirect("/login");
+    } catch (error) {
+      error.error_msg = "註冊失敗 :(";
+      next(error);
+    }
+  })();
 });
 
 module.exports = router;
